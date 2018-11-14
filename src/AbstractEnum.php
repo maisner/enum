54ca5cae970602aa928293e53912173e8aba1253
Michal Šmeidler <smeidler.michal@gmail.com>
2018-11-14 20:50:26 +0100

    Base implementation

---
+++ src/AbstractEnum.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace Maisner\Enum;
+
+/**
+ * Class AbstractEnum
+ * @package Maisner\Enum
+ */
+abstract class AbstractEnum {
+
+	/** @var array|string[] */
+	protected const ALLOWED_VALUES = [];
+
+	/** @var string */
+	protected $value;
+
+	/**
+	 * AbstractEnum constructor.
+	 * @param string $value
+	 */
+	public function __construct(string $value) {
+		if (!$this->checkValue($value)) {
+			$message = \sprintf(
+				'Value "%s" is not allowed. Allowed values [%s]',
+				$value,
+				\implode(', ', $this::allowedValues())
+			);
+
+			throw new NotAllowedValueException($message);
+		}
+		$this->value = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getValue(): string {
+		return $this->value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function __toString(): string {
+		return $this->getValue();
+	}
+
+	/**
+	 * @param string $value
+	 * @return bool
+	 */
+	protected function checkValue(string $value): bool {
+		return \in_array($value, $this::allowedValues(), TRUE);
+	}
+
+	/**
+	 * @return array|string[]
+	 */
+	abstract protected static function allowedValues(): array;
+}
---
+++ src/NotAllowedValueException.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+
+namespace Maisner\Enum;
+
+class NotAllowedValueException extends \InvalidArgumentException {
+
+}
